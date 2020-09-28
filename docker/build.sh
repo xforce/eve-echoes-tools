115c050fd8fd369dbf21787cdb7abd151a9d316e
Alexander Guettler <alexander@guettler.io>
2020-09-28 14:49:08 -0700

    Add optional support for docker buildx, adding support for arm64

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-( cd .. && docker build -f docker/Dockerfile -t evee-tools . )
+
+if docker buildx &> /dev/null; then
+    ( cd .. && docker buildx build --load --platform=amd64,arm64 -f docker/Dockerfile -t cookiemagic/evee-tools:multi . )
+else
+    ( cd .. && docker build -f docker/Dockerfile -t cookiemagic/evee-tools . )
+fi

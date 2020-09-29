cef9df511d58d39530d6a80c38f55732768cc1b8
Alexander Guettler <alexander@guettler.io>
2020-09-29 03:54:48 -0700

    Argh

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if docker buildx &> /dev/null; then
-    ( cd .. && docker buildx build --load --platform=amd64,arm64 -f docker/Dockerfile -t cookiemagic/evee-tools . )
+docker buildx &> /dev/null
+status=$?
+
+if [ "$status" -eq 0 ] &> /dev/null && [ "$1" == "push" ]; then
+    ( cd .. && docker buildx build --push --platform=amd64,arm64 -f docker/Dockerfile -t cookiemagic/evee-tools . )
 else
     ( cd .. && docker build -f docker/Dockerfile -t cookiemagic/evee-tools . )
 fi
